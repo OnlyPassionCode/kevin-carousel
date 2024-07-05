83d24bb1f510b5eaf8c1875498ec030589bf6a61
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-07-06 00:12:25 +0200

    refactoring + examples + remove arrow

---
+++ examples/basic/js/main.js
@@ -0,0 +1,12 @@
+addEventListener('DOMContentLoaded', function(){
+    new KevinCarousel('.kevin-carousel').init({
+        'gap': 20,
+        'draggable': true,
+        'loop': true,
+        'loopTime': 2500,
+        'transitionTime': 500,
+        'pauseLoopOnHover': true,
+        'button': true,
+        'items': 3,
+    });
+})
